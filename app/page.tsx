e922af02cd067ba0b79a91c89ffadad9ccece8ec
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-12 16:37:38 +0100

    restored

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ function HomePage() {
     <div className='flex flex-col items-center justify-center h-screen text-white'>
         <h1 className='text-5xl font-bold mb-20'>ChatGPT</h1>
 
+
         <div className='hidden md:flex space-x-2 text-center' id='notmobile'>
             <div>
                 <div className='flex flex-col items-center justify-center mb-5'>
+                    {/* Sun Icon */}
                     <SunIcon className='h-8 w-8'/>
                     <h2>Examples</h2>
                 </div>
@@ -19,6 +21,38 @@ function HomePage() {
                         "What is the difference between a dog and a cat?"
                     </p>
                     <p className='infoText'>"what is the colour of the sun?"</p>
+                </div>
+            </div>
+
+            {/* second */}
+            <div>
+                <div className='flex flex-col items-center justify-center mb-5'>
+                    <BoltIcon className='h-8 w-8'/>
+                    <h2>Capabilities</h2>
+                </div>
+
+                <div className='space-y-2'>
+                    <p className='infoText'>change the ChatGPT Model to use</p>
+                    <p className='infoText'>
+                        Messages are stored in Firebase's Firestore
+                    </p>
+                    <p className='infoText'>Hot Toast notifications when ChatGPT is thinking!</p>
+                </div>
+            </div>
+
+            {/* third */}
+            <div>
+                <div className='flex flex-col items-center justify-center mb-5'>
+                    <ExclamationTriangleIcon className='h-8 w-8'/>
+                    <h2>Limitations</h2>
+                </div>
+
+                <div className='space-y-2'>
+                    <p className='infoText'>May generate incorrect info</p>
+                    <p className='infoText'>
+                        May produce harful instructions
+                    </p>
+                    <p className='infoText'>Limited knowledge of world and events after 2021</p>
                 </div>
             </div>
         </div>
