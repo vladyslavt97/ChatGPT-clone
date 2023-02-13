1e4f028d50e7a45d4a7a57253026e1a82cee8fd4
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-13 11:55:52 +0100

    Revert "restored"
    This reverts commit e922af02cd067ba0b79a91c89ffadad9ccece8ec.

--- app/page.tsx
+++ app/page.tsx
@@ -21,38 +21,6 @@ function HomePage() {
                     <p className='infoText'>"what is the colour of the sun?"</p>
                 </div>
             </div>
-
-            {/* second */}
-            <div>
-                <div className='flex flex-col items-center justify-center mb-5'>
-                    <BoltIcon className='h-8 w-8'/>
-                    <h2>Capabilities</h2>
-                </div>
-
-                <div className='space-y-2'>
-                    <p className='infoText'>change the ChatGPT Model to use</p>
-                    <p className='infoText'>
-                        Messages are stored in Firebase's Firestore
-                    </p>
-                    <p className='infoText'>Hot Toast notifications when ChatGPT is thinking!</p>
-                </div>
-            </div>
-
-            {/* third */}
-            <div>
-                <div className='flex flex-col items-center justify-center mb-5'>
-                    <ExclamationTriangleIcon className='h-8 w-8'/>
-                    <h2>Limitations</h2>
-                </div>
-
-                <div className='space-y-2'>
-                    <p className='infoText'>May generate incorrect info</p>
-                    <p className='infoText'>
-                        May produce harful instructions
-                    </p>
-                    <p className='infoText'>Limited knowledge of world and events after 2021</p>
-                </div>
-            </div>
         </div>
     </div>
   )
