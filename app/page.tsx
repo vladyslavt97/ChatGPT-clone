faf7a75bde310f85a7763a7d3fdf997c1e7f057b
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-13 11:51:16 +0100

    a few changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,9 @@ function HomePage() {
     <div className='flex flex-col items-center justify-center h-screen text-white'>
         <h1 className='text-5xl font-bold mb-20'>ChatGPT</h1>
 
-
         <div className='hidden md:flex space-x-2 text-center' id='notmobile'>
             <div>
                 <div className='flex flex-col items-center justify-center mb-5'>
-                    {/* Sun Icon */}
                     <SunIcon className='h-8 w-8'/>
                     <h2>Examples</h2>
                 </div>
