222a9584c825f4dfc0f3e91168eaf54de9990e40
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-14 12:19:01 +0100

    signin with fb and github

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,17 +1,29 @@
 'use client'
 import { signIn } from "next-auth/react"
 import Image from "next/image"
+import fb from '../public/fb.png'
+import google from '../public/google.png'
+import github from '../public/github.png'
 
 function Login() {
   return (
     <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+        <h1 className="text-black">Recreated by Vladyslav Tsurkanenko😊</h1>
         <Image 
         src="https://links.papareact.com/2i6"
         width={300}
         height={300}
         alt="logo"
         />
-        <button onClick={()=>signIn('google')} className="text-white font-bold text-3xl animate-pulse">Sign in to use ChatGPT</button>
+        <button onClick={()=>signIn('facebook')} className="text-white font-bold text-3xl animate-pulse flex flex-row items-center bg-blue-500 px-5 py-2 rounded-lg">Signin with &nbsp;
+          <Image src={fb} alt="fb" width={50} height={50}/>
+        </button>
+        <button onClick={()=>signIn('google')} className="text-white font-bold text-3xl animate-pulse flex flex-row items-center mt-5 bg-slate-600 px-5 py-2 rounded-lg">Sign in with &nbsp;
+          <Image src={google} alt="fb" width={100} height={100}/>
+        </button>
+        <button onClick={()=>signIn('github')} className="text-white font-bold text-3xl animate-pulse flex flex-row items-center mt-5 bg-slate-600 px-5 py-2 rounded-lg">Sign in with &nbsp;
+          <Image src={github} alt="fb" width={80} height={80}/>
+        </button>
     </div>
   )
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
+
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -8,6 +12,14 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
     // ...add more providers here
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    })
   ],
 }
 export default NextAuth(authOptions)
